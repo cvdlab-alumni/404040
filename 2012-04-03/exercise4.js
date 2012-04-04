3d940236167aab56bd0ff3746902642c4fb9c008
Dario D'agostino <dario.dago@gmail.com>
2012-04-04 17:38:24 +0200

    ultimo esercizio

---
+++ 2012-04-03/exercise4.js
@@ -0,0 +1,101 @@
+var pezzo1 = SIMPLEX_GRID([[39],[1],[1]]);
+var pezzo2 = T([1])([1])(SIMPLEX_GRID([[1],[1],[1]]));
+var pezzo3 = T([0,1])([21,1])(SIMPLEX_GRID([[15],[3],[1]]));
+var pezzo4 = T([0,1])([21,4])(SIMPLEX_GRID([[31],[1],[1]]));
+var pezzo5 = T([0,1])([51,5])(SIMPLEX_GRID([[1],[1],[1]]));
+var pezzo6 = T([0,1])([39,5])(SIMPLEX_GRID([[8],[11],[1]]));
+var pezzo7 = T([0,1])([21,5])(SIMPLEX_GRID([[18],[12],[1]]));
+var pezzo8 = T([0,1])([1,10])(SIMPLEX_GRID([[20],[7],[1]]));
+var pezzo9 = T([0,1])([1,17])(SIMPLEX_GRID([[8],[5],[1]]));
+var floor3D = STRUCT([pezzo1, pezzo2, pezzo3,
+					pezzo4, pezzo5, pezzo6,
+					pezzo7, pezzo8, pezzo9]);
+//DRAW(floor3D);
+
+
+var piscinaGrande = T([0,1])([1,1])(SIMPLEX_GRID([[20],[9],[0.6]]));
+var piscinaPiccola = T([0,1])([47,5])(SIMPLEX_GRID([[4],[11],[0.6]]));
+var piscine3D = STRUCT([piscinaGrande, piscinaPiccola]);
+//DRAW(piscine3D);
+
+
+var scalino1 = T([0,1])([36,1])(SIMPLEX_GRID([[0.5],[3],[1]]));
+var scalino2 = T([0,1])([36+0.5,1])(SIMPLEX_GRID([[0.5],[3],[0.8]]));
+var scalino3 = T([0,1])([37,1])(SIMPLEX_GRID([[0.5],[3],[0.6]]));
+var scalino4 = T([0,1])([37+0.5,1])(SIMPLEX_GRID([[0.5],[3],[0.4]]));
+var scalino5 = T([0,1])([38,1])(SIMPLEX_GRID([[0.5],[3],[0.2]]));
+var scalino6 = T([0,1])([38+0.5,1])(SIMPLEX_GRID([[0.5],[3],[0]]));
+var scala3D = STRUCT([scalino1, scalino2, scalino3, scalino4, scalino5, scalino6]);
+//DRAW(scala3D);
+
+
+var muroVicinoPanchina = T([0,1,2])([7.5,15,1])(SIMPLEX_GRID([[19],[0.25],[3]]));
+var muro1 = T([0,1,2])([1,1-0.25,1])(SIMPLEX_GRID([[6.5],[0.25],[3]]));
+var muro2 = T([0,1,2])([1-0.25,1-0.25,0])(SIMPLEX_GRID([[0.25],[21.5],[4]]));
+var muro3 = T([0,1,2])([1-0.25,22,0])(SIMPLEX_GRID([[8.5],[0.25],[4]]));
+var muro4 = T([0,1,2])([9,17,0])(SIMPLEX_GRID([[0.25],[5],[4]]));
+var muro5 = T([0,1,2])([25,7.5-0.25,1])(SIMPLEX_GRID([[9],[0.25],[3]]));
+var muro6 = T([0,1,2])([37.8,16,0])(SIMPLEX_GRID([[13.2],[0.25],[4]]));
+var muro7 = T([0,1,2])([51,5-0.25,0])(SIMPLEX_GRID([[0.25],[11.5],[4]]));
+var muro8 = T([0,1,2])([41.2,5-0.25,1])(SIMPLEX_GRID([[9.8],[0.25],[3]]));
+var muro9 = T([0,1,2])([37.2,11.4,1])(SIMPLEX_GRID([[5],[0.25],[3]]));
+var muri3D = STRUCT([muroVicinoPanchina, muro1, muro2, muro3, muro4, muro5, muro6, muro7, muro8, muro9]);
+//DRAW(muri3D);
+
+
+var vetrata1 = T([0,1,2])([31,7.5,1])(SIMPLEX_GRID([[0.25],[6],[3]]));
+var vetrata2 = T([0,1,2])([32,7.5,1])(SIMPLEX_GRID([[0.25],[6],[3]]));
+var vetrata3 = T([0,1,2])([30,13.5,1])(SIMPLEX_GRID([[10],[0.25],[3]]));
+var vetrata4 = T([0,1,2])([44.6,6.8,1])(SIMPLEX_GRID([[0.25],[7.3],[3]]));
+var vetrata5 = T([0,1,2])([30,5-0.25,1])(SIMPLEX_GRID([[11.2],[0.25],[3]]));
+var vetrata6 = T([0,1,2])([39,13.5+0.25,1])(SIMPLEX_GRID([[0.25],[2.25],[3]]));
+var vetrate3D = STRUCT([vetrata1,vetrata2,vetrata3,vetrata4,vetrata5,vetrata6]);
+//DRAW(vetrate3D);
+
+
+var tettoGrande = T([0,1,2])([24,4,4])(SIMPLEX_GRID([[23],[13],[0.4]]));
+var tettoPiccolo = T([0,1,2])([0,13,4])(SIMPLEX_GRID([[10],[10],[0.4]]));
+var tetti3D = STRUCT([tettoGrande, tettoPiccolo]);
+//DRAW(tetti3D);
+
+
+var colonne1 = T([0,1,2])([26,7,1])(SIMPLEX_GRID([REPLICA(4)([0.15, -6.2]), [0.15],	[3]]));
+var colonne2 = T([0,1,2])([26,13,1])(SIMPLEX_GRID([REPLICA(4)([0.15, -6.2]), [0.15], [3]]));
+var colonne3D = STRUCT([colonne1, colonne2]);
+//DRAW(colonne3D);
+
+
+var piediPanchina = T([0,1,2])([7,14.15,1])(SIMPLEX_GRID([REPLICA(8)([0.2, -2]), [0.2], [0.4]]));
+var basePanchina = T([0,1,2])([7,14,1+0.4])(SIMPLEX_GRID([[15.6], [0.5], [0.15]]));
+var panchina3D = STRUCT([piediPanchina, basePanchina]);
+//DRAW(panchina3D);
+
+COLOR([1,0.9,0.6])(floor3D);
+COLOR([0.2,0.4,0.6])(piscine3D);
+COLOR([0.9,0.8,0.6])(scala3D);
+COLOR([0.4,0.4,0.4])(muri3D);
+COLOR([0,0,0])(tetti3D);
+COLOR([0,0,0])(colonne3D);
+COLOR([0.8,0.8,0.8])(panchina3D);
+COLOR([0.2,0.6,1])(vetrate3D);
+//COLOR di tutte le struct
+
+var generaSgabello = function(){
+	var parteAlta = T([2])([0+0.5])(SIMPLEX_GRID([[1],[1],[0.05]]));
+	var gamba1 = T([0,1])([0.2,0.45])(SIMPLEX_GRID([[0.1],[0.1],[0.5]]));
+	var gamba2 = T([0,1])([0.7,0.45])(SIMPLEX_GRID([[0.1],[0.1],[0.5]]));
+	return 	COLOR([1,0.1,0.1])(STRUCT([parteAlta,gamba2,gamba1]));
+};
+//permette di istanziare più sgabelli
+
+var sgabello1 = T([0,1,2])([42,8,1])(generaSgabello());
+var sgabello2 = T([0,1,2])([42,9,1])(generaSgabello());
+var sgabello3 = T([0,1,2])([33,8,1])(generaSgabello());
+var sgabello4 = T([0,1,2])([33,11,1])(generaSgabello());
+var sgabello5 = T([0,1,2])([2,18,1])(generaSgabello());
+var sgabello6 = T([0,1,2])([4,18,1])(generaSgabello());
+var sgabello7 = T([0,1,2])([6,18,1])(generaSgabello());
+var sgabelli3D = STRUCT([sgabello1,sgabello2,sgabello3,sgabello4,sgabello5,sgabello6,sgabello7]);
+
+var pavilionFinale = STRUCT([floor3D, piscine3D, scala3D, muri3D, tetti3D, colonne3D, panchina3D, vetrate3D,sgabelli3D]);
+DRAW(pavilionFinale);
